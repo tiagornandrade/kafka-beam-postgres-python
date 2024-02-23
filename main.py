c4b49137c16c62c501070fcc13b5aa25751247dc
tiagornandrade <tiagornandrade@hotmail.com>
2024-02-23 16:22:57 -0300

    feat: Ajusta projeto

--- main.py
+++ main.py
@@ -26,49 +26,63 @@ class WriteToPostgres(beam.DoFn):
     def process(self, element):
         cursor = self.conn.cursor()
         try:
-            cursor.execute("""
+            cursor.execute(
+                """
                 INSERT INTO financial_transactions (
                     transaction_id, product_id, product_name, product_category,
                     product_price, product_quantity, product_brand, currency,
                     customer_id, transaction_date, payment_method, total_amount
                 ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-            """, (
-                element['transactionId'], element['productId'], element['productName'],
-                element['productCategory'], element['productPrice'], element['productQuantity'],
-                element['productBrand'], element['currency'], element['customerId'],
-                element['transactionDate'], element['paymentMethod'], element['totalAmount']
-            ))
+            """,
+                (
+                    element["transactionId"],
+                    element["productId"],
+                    element["productName"],
+                    element["productCategory"],
+                    element["productPrice"],
+                    element["productQuantity"],
+                    element["productBrand"],
+                    element["currency"],
+                    element["customerId"],
+                    element["transactionDate"],
+                    element["paymentMethod"],
+                    element["totalAmount"],
+                ),
+            )
             self.conn.commit()
         except Exception as e:
             print(e)
         finally:
             cursor.close()
 
+
 def run():
     options = PipelineOptions()
     p = beam.Pipeline(options=options)
 
     consumer_config = {
-        'topic': 'financial_transactions',
-        'bootstrap_servers': 'localhost:9092',
-        'group_id': 'beam-consumer-group'
+        "topic": "financial_transactions",
+        "bootstrap_servers": "localhost:9092",
+        "group_id": "beam-consumer-group",
     }
 
     connection_options = {
-        'host': 'localhost',
-        'port': '5432',
-        'database': 'postgres',
-        'user': 'postgres',
-        'password': 'postgres'
+        "host": "localhost",
+        "port": "5432",
+        "database": "postgres",
+        "user": "postgres",
+        "password": "postgres",
     }
 
-    (p
-     | 'ReadFromKafka' >> KafkaConsume(
-                 consumer_config=consumer_config,
-            )
-     | 'ExtractAndTransform' >> beam.ParDo(ExtractAndTransform())
-     | 'WriteToPostgres' >> beam.ParDo(WriteToPostgres(connection_options))
-     )
+    (
+        p
+        | "ReadFromKafka"
+        >> KafkaConsume(
+            consumer_config=consumer_config,
+        )
+        | "ExtractAndTransform" >> beam.ParDo(ExtractAndTransform())
+        | "WriteToPostgres" >> beam.ParDo(WriteToPostgres(connection_options))
+    )
 
     result = p.run()
     result.wait_until_finish()
